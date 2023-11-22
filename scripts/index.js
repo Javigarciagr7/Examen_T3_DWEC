316f4aed74c79aa499c25597dd96d41737b9b85a
Javier García <javiergarciarodriguez@alumnos.tuniversformacion.es>
2023-11-22 08:52:27 +0100

    El primer error encontrado era un innerHeight que estaba escrito 2 veces en vez de innerWidth

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerHeight;
+// 1º ERROR, hay escritos 2 innerHeight cuando uno de ellos no corresponde y debe ser innerWidth
+const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 function random(min, max) {
