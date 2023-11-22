3233b15409a466ceb2f6a6f049dccdf56b5480de
Javier García <javiergarciarodriguez@alumnos.tuniversformacion.es>
2023-11-22 09:46:44 +0100

    Ya estan los archivos js creados para la modulación, ahora queda crear los imports y exports

---
+++ scripts/ballclass.js
@@ -0,0 +1,53 @@
+// 2º ERROR, en el contructor de la clase ball falta el parametro color y sustituir el randomRGB() por color en el this.color = 
+class Ball {
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+
+
+// 5ª ERROR, modificar el Math.PI() y quitarle los parentesis
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    update() {
+        if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+            this.velX = -this.velX;
+        }
+
+        if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+            this.velY = -this.velY;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    collisionDetect(otherBall) {
+        const dx = this.x - otherBall.x;
+        const dy = this.y - otherBall.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + otherBall.size) {
+            otherBall.color = this.color = randomRGB();
+        }
+    }
+}
+
+const balls = [];
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
---
+++ scripts/canvas_setup.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// 1º ERROR, hay escritos 2 innerHeight cuando uno de ellos no corresponde y debe ser innerWidth
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
